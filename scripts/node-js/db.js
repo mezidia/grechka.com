d4082eadc17327d9cb10f20717302b04a410fbfa
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-01-20 20:04:34 +0000

    #2 Create db.js

---
+++ scripts/node-js/db.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const sqlite3 = require('sqlite3').verbose();
+
+// open database in memory
+let db = new sqlite3.Database('products.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+// close the database connection
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
