ba0f08337ac1f9c221283c9b4d1ffbf33bef81e3
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-01-20 20:11:14 +0000

    #2 Write connect and close as functions

--- scripts/node-js/db.js
+++ scripts/node-js/db.js
@@ -2,18 +2,29 @@
 
 const sqlite3 = require('sqlite3').verbose();
 
-// open database in memory
-let db = new sqlite3.Database('products.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the in-memory SQlite database.');
-});
+const connect = (path) => {
+  // open database in memory
+  const db = new sqlite3.Database(path, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+    return db;
+  });
+};
 
-// close the database connection
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
+const close = (db) => {
+  // close the database connection
+  db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
+  // ** Maybe we can return True here ** //
+};
+
+export default {
+  connect,
+  close,
+}
