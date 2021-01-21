30362e4a843d6992bd7c4c98cd314c65836c425f
liza <gorbunova.elizaveta@gmail.com>
2021-01-21 11:25:59 +0200

    create simple grabber for rozetka

--- scripts/node-js/grabber.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-//here we write grabber
-
-//rozetka api
-//https://api.seller.rozetka.com.ua/apidoc/#api-Items-GetItemSearch
---
+++ scripts/node-js/rozetkaGrabber.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const cheerio = require('cheerio');
+const request = require('request');
+
+let n = 0;
+
+grab('https://rozetka.com.ua/ua/krupy/c4628397/sort=cheap;vid-225787=grechka/');
+
+function grab(site, page) {
+  request(site, (error, response, html) => {
+    if (!error && response.statusCode === 200) return;
+    const $ = cheerio.load(html);
+    $('.goods-tile').each((i, el) => {
+      if (n > 10) return;
+      const title = $(el).find('.goods-tile__title').text();
+      const link = $(el).find('.goods-tile__picture').attr('href');
+      const img = $(el).find('.lazy_img_hover').attr('src');
+      const price = $(el).find('.goods-tile__price-value').text();
+      console.log(title, link, img, price);
+      n++;
+    });
+  });
+}
