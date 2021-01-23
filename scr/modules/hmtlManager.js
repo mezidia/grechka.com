c01d7902deb3f56659553803f421d4bf5a1a56ad
La7rodectus <maxmega27@gmail.com>
2021-01-23 12:16:22 +0200

    htmlManager class

---
+++ scr/modules/hmtlManager.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export default class htmlManager {
+  constructor(document) {
+    this.doc = document;
+  }
+}
+
+
+
--- scr/script.js
+++ scr/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
+import htmlManager from "./modules/hmtlManager.js";
 
 
+const html = new htmlManager();
 
-
+console.log(html);
