2e513ab3e445360187895a5d2ac76a55028a8bdc
La7rodectus <maxmega27@gmail.com>
2021-01-24 00:17:49 +0200

    filters js

--- scr/modules/hmtlManager_class.js
+++ scr/modules/hmtlManager_class.js
@@ -5,6 +5,7 @@ import ProductCell from "./productCell_class.js";
 
 export default class HtmlManager {
   _productsPlaceHolder = document.getElementById('productsPlaceholder-wraper');
+  _products = [];
   constructor() {
     if (!HtmlManager._instance) {
       HtmlManager._instance = this;
@@ -13,13 +14,30 @@ export default class HtmlManager {
   }
 
   clearProducts() {
+    _products = [];
     this._productsPlaceHolder.innerHTML = '';
   }
 
   addProduct(prodData) {
-    console.log(prodData);
-    this._productsPlaceHolder.innerHTML += new ProductCell(prodData).getElmenHtml();
+    const productID = this._products.length;
+    const product = new ProductCell(prodData, productID);
+    this._productsPlaceHolder.innerHTML += product.getElmentHtml();
+    const htmlElement = document.getElementById(productID);
+    product.htmlElement = htmlElement;
+    this._products.push(product);
+    console.log(product);
   }
+
+  filter(ruleCB) {
+    for (const product of this._products) {
+      console.log(product);
+      if (ruleCB(product.weight)) {
+        product.htmlElement.style.display = 'none';
+      }
+    }
+  }
+
+
 }
 
 
--- scr/script.js
+++ scr/script.js
@@ -1,8 +1,11 @@
 'use strict';
 
+import FilterCheckbox from "./modules/filterCheckbox_class.js";
 import HtmlManager from "./modules/hmtlManager_class.js";
 
 const html = new HtmlManager(1);
+
+
 
 console.log(html);
 
@@ -14,6 +17,19 @@ const prod = {
   'prodURL': 'prodURL',
   'price': 'price',
   'description': 'description',
+  'weight': '1300',
+}
+
+console.log(document.getElementsByClassName('filter-checkbox'));
+const checkboxs = document.getElementsByClassName('filter-checkbox');
+
+for (const checkbox of checkboxs) {
+  checkbox.onclick = (event) => {
+    console.log(event.target);
+    console.log(new FilterCheckbox(event.target.id).rule);
+    html.filter(new FilterCheckbox(event.target.id).rule);
+  };
 }
 
 html.addProduct(prod);
+
